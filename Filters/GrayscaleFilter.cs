779d285e2b90b165c13973dc2540b7b6c88f2dd8
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2019-05-16 12:36:48 +0300

    Task 2.3 done. Created by abstract base class PixelFilter

--- Filters/GrayscaleFilter.cs
+++ Filters/GrayscaleFilter.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace MyPhotoshop
 {
-    public class GrayscaleFilter : IFilter
+    public class GrayscaleFilter : PixelFilter
     {
-        public ParameterInfo[] GetParameters()
+        public override ParameterInfo[] GetParameters()
         {
             return new ParameterInfo[0];
         }
@@ -14,20 +14,7 @@ namespace MyPhotoshop
             return "Оттенки серого";
         }
 
-        public Photo Process(Photo original, double[] parameters)
-        {
-            var result = new Photo(original.width,original.height);
-
-            for (int x = 0; x < result.width; x++)
-                for (int y = 0; y < result.height; y++)
-                {
-                    result[x, y] = ProcessPixel(original[x, y], parameters);
-                }
-
-            return result;
-        }
-
-        public Pixel ProcessPixel(Pixel original, double[] parameters)
+        public override Pixel ProcessPixel(Pixel original, double[] parameters)
         {
             var lightness = original.R + original.G + original.B;
             lightness /= 3;
