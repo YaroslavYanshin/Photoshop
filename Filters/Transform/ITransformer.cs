4a6e6f9a90ca3e2062b25318e9ad32cfa5d5b682
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2019-06-12 23:39:29 +0300

    Changes to task 3.3

---
+++ Filters/Transform/ITransformer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public interface ITransformer<TParameters> where TParameters:IParameters,new()
+    {
+        void Prepare(Size size, TParameters parameters);
+        Size ResualtSize { get; }
+        Point? MapPoint(Point newPoint);
+    }
+}
---
+++ Filters/Transform/TransformFilter{TParameters}.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class TransformFilter <TParameters> : ParametrizedFilter<TParameters> where TParameters : IParameters,new ()
+    {
+        ITransformer<TParameters> transformer;
+        string name;
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public TransformFilter(string name, ITransformer<TParameters> transformer)
+        {
+            
+            this.name = name;
+            this.transformer = transformer;
+        }
+
+        public override Photo Process(Photo original, TParameters parameters)
+        {
+            var oldSize = new Size(original.width, original.height);
+            transformer.Prepare(oldSize, parameters);
+            var result = new Photo(transformer.ResualtSize.Width, transformer.ResualtSize.Height);
+            for (int x = 0; x < result.width; x++)
+                for (int y = 0; y < result.height; y++)
+                {
+                    var point = new Point(x, y);
+                    var oldPoint = transformer.MapPoint(point);
+                    if(oldPoint.HasValue)
+                    result[x, y] = original[oldPoint.Value.X, oldPoint.Value.Y];
+                }
+            return result;
+        }
+    }
+}
