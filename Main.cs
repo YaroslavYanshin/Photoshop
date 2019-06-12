b2aa8f8d99a112fceccd7d0a7efcafce85105050
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2019-06-12 14:31:08 +0300

    Task 3.2 done. Add class TransformFilter

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyPhotoshop
@@ -21,8 +22,18 @@ namespace MyPhotoshop
                     var lightness = original.R + original.G + original.B;
                     lightness /= 3;
                     return new Pixel(lightness, lightness, lightness);
-                }  
-                ));
+                }  ));
+
+            window.AddFilter(new TransformFilter(
+                "Отразить по горизонтали",
+                size => size,
+                (point, size) => new Point(size.Width - point.X-1, point.Y)));
+
+            window.AddFilter(new TransformFilter(
+                "Повернуть против ч.с.",
+                size => new Size(size.Height, size.Width),
+                (point, size) => new Point(point.Y, point.X)));
+
 			Application.Run (window);
 		}
 	}
